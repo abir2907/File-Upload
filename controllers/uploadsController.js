2fbeccab837bc2f2e867fcbb2081b295dacac5f0
Abir Chodha <abirchodha1@gmail.com>
2025-10-08 01:41:51 +0530

    Add additional image checks

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,27 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+  // check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file uploaded");
+  }
+
   const productImage = req.files.image;
+
+  // check format
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload image");
+  }
+
+  // check image size
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please upload image smaller than 1MB"
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
