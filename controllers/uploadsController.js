0c9710dcba90a7928138382a584088f51bfbc4e6
Abir Chodha <abirchodha1@gmail.com>
2025-10-08 01:16:59 +0530

    Extract image path and save image in server while making it public

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,19 @@
 const { StatusCodes } = require("http-status-codes");
+const path = require("path");
 
 const uploadProductImage = async (req, res) => {
-  console.log(req.files);
-  res.send("upload product image");
+  const productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = { uploadProductImage };
